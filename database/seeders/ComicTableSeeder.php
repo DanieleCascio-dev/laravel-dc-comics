00578b5919a8ec0480216ecb862484ae1b799dd6
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2024-01-18 15:40:39 +0100

    project set up

---
+++ database/seeders/ComicTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comic;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $comic= new Comic();
+    }
+}
