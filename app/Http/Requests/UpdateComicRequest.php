3716177040515540f469901c2f4c0dda1af7e908
DanieleCascio-dev <danielecascio2018@gmail.com.com>
2024-01-22 16:07:57 +0100

    add. UpdateComicRequest

---
+++ app/Http/Requests/UpdateComicRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateComicRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'title'=>'required|min:4|max:100',
+            'description'=>'required',
+            'image'=>'required',
+            'price'=>'required',
+            'sale_date'=>'required|date_format:Y-m-d',
+            'type'=>'required',
+            'series'=>'required'
+        ];
+    }
+
+    public function messages(){
+
+        return
+        [
+            'title.required'=>'A title is nedeed to proceed.',
+            'title.min'=>'The title must include at least 4 characters',
+            'description.required'=>'You have to add a description to a new comic.',
+            'image.required'=>'Image required to a new comic',
+            'price.required'=>'You need to add a price.',
+            'sale_date.required'=>'You need to add a date',
+            'sale_date.date_format'=>'You need to enter a Y-m-d date format',
+            'type.required'=>'You need to select a type',
+            'series.required'=>'You need to specify the series'
+        ];
+
+    }
+}
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -5,6 +5,17 @@
     
     <h1>Modifica: {{$comic->title}}</h1>
 
+    @if ($errors->any())
+      <div class="alert alert-danger mt-3">
+        <ul>
+          @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+                
+          @endforeach
+      </ul>
+      </div>
+    @endif
+
     <form action="{{route('comics.update', ['comic'=>$comic->id]) }}" method="POST">
       @csrf
       @method('PUT')
@@ -13,27 +24,27 @@
   
           <div class="mb-3">
             <label for="title" class="form-label">Title</label>
-            <input type="text" class="form-control" id="title" name="title" value="{{ $comic->title }}" >
+            <input type="text" class="form-control" id="title" name="title" value="{{ old('title') ?? $comic->title }}" >
           </div>
   
           <div class="mb-3">
             <label for="image" class="form-label">Image</label>
-            <input type="text" class="form-control" id="image" name="image" value="{{ $comic->image }}" >
+            <input type="text" class="form-control" id="image" name="image" value="{{ old('image') ?? $comic->image }}" >
           </div>
   
           <div class="mb-3">
             <label for="series" class="form-label">Series</label>
-            <input type="text" class="form-control" id="series" name="series" value="{{ $comic->series}}" >
+            <input type="text" class="form-control" id="series" name="series" value="{{ old('series') ?? $comic->series}}" >
           </div>
   
           <div class="mb-3">
             <label for="sale_date" class="form-label">Sale date</label>
-            <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{ $comic->sale_date}}" >
+            <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{ old('sale_date') ?? $comic->sale_date}}" >
           </div>
   
           <div class="mb-3">
             <label for="price" class="form-label">Price</label>
-            <input type="text" class="form-control" id="price" name="price" value="{{ $comic->price }}">
+            <input type="text" class="form-control" id="price" name="price" value="{{ old('price') ?? $comic->price }}">
           </div>
   
           <div class="mb-3">
@@ -41,14 +52,14 @@
             {{-- <input type="text" class="form-control" id="type" name="type" > --}}
             <select name="type" id="type">
               <option value="">Select one type</option>
-              <option @selected($comic->type === 'graphic novel') value="graphic novel">Graphic novel</option>
-              <option @selected($comic->type === 'comic book') value="comic book">Comic Book</option>
+              <option @selected(old('type', $comic->type) === 'graphic novel') value="graphic novel">Graphic novel</option>
+              <option @selected(old('type',$comic->type) === 'comic book') value="comic book">Comic Book</option>
             </select>
           </div>
   
           <div class="form">
             <label for="description">Description</label>
-            <textarea class="form-control" id="description" name="description" style="height: 100px"> {{ $comic->description }}</textarea>
+            <textarea class="form-control" id="description" name="description" style="height: 100px"> {{ old('description') ?? $comic->description }}</textarea>
           </div>
   
   
